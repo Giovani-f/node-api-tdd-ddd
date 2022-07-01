14839515d96c3934860bc532495b0eb0fd7c74ae
Giovani Fernandes <gifernandes105@gmail.com>
2022-07-01 11:56:59 -0300

    feat: ensure FacebookAPI returns undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '722497688817577',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '79fb7a716698a92e3a1ff58d9230e1af'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
+describe('Facebook Api Integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'EAAKRG5oXz6kBAHjrqXD7xCBVGpXZAg9ijwBJ6SlTPJgMn04KUAgI8ioEfU14wHTWVs20m1BAT3vYF8ev1K2fJv2YbFbYbZA6CNSwU84BPDj1yUKAv6f0xQjBZBKrEpCFt7LGSbHsTbKPqZANZBuifJTAN0BtVsJTQ0lMPNyIu4W10yVZAxin2KLO4xUoGFvbrClGOnhMRVazSi7JsyScsJ' })
+
+    expect(fbUser).toEqual({
+      facebookId: '100954482684294',
+      email: 'mango_pfiylou_curso@tfbnw.net',
+      name: 'Mango Curso'
+    })
+  })
+
+  it('should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
