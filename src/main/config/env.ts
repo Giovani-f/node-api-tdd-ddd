4a2f5d5161bc450c61819b162864e07456fdf6b7
Giovani Fernandes <gifernandes105@gmail.com>
2022-07-13 10:30:23 -0300

    chore: configure express setup

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '722497688817577',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '79fb7a716698a92e3a1ff58d9230e1af'
-  }
+  },
+  port: process.env.PORT ?? 8080
 }
