8089d8c1343d3888db17d5b39e86eb313fcd7f86
Giovani Fernandes <gifernandes105@gmail.com>
2022-07-22 11:26:35 -0300

    test: ensure JwtTokenHandler returns correct key

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -55,9 +55,12 @@ describe('JwtTokenHandler', () => {
 
   describe('validateToken', () => {
     let token: string
+    let key: string
 
     beforeAll(() => {
       token = 'any_token'
+      key = 'any_any'
+      fakeJwt.verify.mockImplementation(() => ({ key }))
     })
 
     it('should call sign with correct params', async () => {
@@ -65,6 +68,12 @@ describe('JwtTokenHandler', () => {
 
       expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret)
       expect(fakeJwt.verify).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return the key used to sign', async () => {
+      const generatedKey = await sut.validateToken({ token })
+
+      expect(generatedKey).toBe(key)
     })
   })
 })
